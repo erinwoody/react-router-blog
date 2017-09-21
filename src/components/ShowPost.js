19c450e4af23528664192ab57fef39774143518f
Erin Woody <erinelizabethwoody@gmail.com>
2017-09-21 16:55:33 -0500

    added show post

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import PostList from './PostList';
 
 export default class ShowPost extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+}
+
+render() {
+    let posts;
+    posts = this.props.posts.map((post, index) => {
+        return (
+            <div key={index}>
+                <p>Added By: {post.userName}</p>
+                <p>Artist/Band: {post.songArtist}</p>
+                <p>Title: {post.songTitle}</p>
+                <p>Comments: {post.songNotes}</p>
+            </div>
+        )
+    })
+
     return (
-      <div>
-      <h1> Show all posts!</h1>
-      </div>
-    )
-  }
+        <div className="">{posts}</div>
+    );
+
+}
 }
